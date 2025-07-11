80b3800b634eb64aa994adec9e320ce9ce0d8eec
Ken <ken@foxrunholdings.com>
2025-07-10 18:05:42 -0700

    Replace layout component with div in teams view
    The <x-flux:layout> Blade component was replaced with a standard <div> in the teams.blade.php view. This change may be for simplification or to remove dependency on the custom layout component.

--- src/resources/views/teams.blade.php
+++ src/resources/views/teams.blade.php
@@ -1,4 +1,4 @@
-<x-flux:layout>
+<div>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Teams') }}
@@ -14,4 +14,4 @@
             </div>
         </div>
     </div>
-</x-flux:layout>
+</div>
